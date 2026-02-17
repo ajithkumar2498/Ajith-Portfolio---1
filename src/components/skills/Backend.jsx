5b2361cd75ec8e27b5c27a9669d17a1b16d3f1c8
ajithkumar2498 <ajibhc24@gmail.com>
2026-02-18 00:03:03 +0530

    n

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -28,7 +28,7 @@ const Backend = () => {
             <i class='bx bx-badge-check'></i>
 
             <div>
-                <h3 className="skills__name">Mysql</h3>
+                <h3 className="skills__name">MySQL</h3>
                 <span className="skills__level">Intermediate</span>
             </div>
             </div>
